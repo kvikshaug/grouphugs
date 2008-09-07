3d99d20a24074c550eadb4873d350979b31894e3
murray <murray@c6f41995-d01f-4a11-924a-98aa25776321>
2008-09-07 17:48:13 +0000

    First commit on the start of a new module; SVNOutput. See file for more info. This commit also helps testing the post-commit hook.
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@39 c6f41995-d01f-4a11-924a-98aa25776321

---
+++ trunk/grouphug/SVNOutput.java
@@ -0,0 +1,16 @@
+package grouphug;
+
+/**
+ * SVNOutput: A module that parses and outputs information about an SVN commit.
+ */
+// TODO: maybe this shouldn't implement GHModule, since it's not triggered by trigger() ?
+public class SVNOutput implements GrouphugModule {
+
+    public SVNOutput() {
+        
+    }
+
+    public void trigger(Grouphug bot, String channel, String sender, String login, String hostname, String message) {
+        // do nothing
+    }
+}
