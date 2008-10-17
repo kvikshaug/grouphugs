d9cbb9a15558af9179a3878379fee0e5552515fd
sunn <sunn@c6f41995-d01f-4a11-924a-98aa25776321>
2008-10-17 13:50:52 +0000

    Minor change to the confession hack.
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@84 c6f41995-d01f-4a11-924a-98aa25776321

--- trunk/grouphug/Confession.java
+++ trunk/grouphug/Confession.java
@@ -113,11 +113,11 @@ class Confession implements GrouphugModule {
             BufferedReader gh = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
 
             // We dig down to the confession content
-            // HACK: Skip to the third content div - as the (currently) three newsitems also use this layout
+            // HACK: Skip to the fourth content div - as the (currently) four newsitems also use this layout
             int i=0;
             while((line = gh.readLine()) != null) {
                 if(line.equals("  <div class=\"content\">")) {
-                    if(i==3)
+                    if(i==4)
                         break;
                     else
                         i++;
