d00f7f1f9c5fc7ef1fa570db289ea86b24dfa1d9
krashk <krashk@c6f41995-d01f-4a11-924a-98aa25776321>
2008-12-08 20:24:23 +0000

    I'm so excited, and I just can't hide it.
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@209 c6f41995-d01f-4a11-924a-98aa25776321

--- trunk/grouphug/modules/WordCount.java
+++ trunk/grouphug/modules/WordCount.java
@@ -8,8 +8,11 @@ import grouphug.SQL;
 
 public class WordCount implements GrouphugModule {
 	private static Grouphug bot;
-	
-	private final String TRIGGER = "Wordcount";
+
+    private static final String DEFAULT_SQL_HOST = "127.0.0.1";
+    private static final String DEFAULT_SQL_USER = "gh";
+    private final String TRIGGER_HELP = "wordcount";
+	private final String TRIGGER = "wordcount ";
 	private final String WORDS_DB= "gh_words";
 
 	
@@ -24,7 +27,7 @@ public class WordCount implements GrouphugModule {
 		
 	
 		try{
-			sql.connect();
+			sql.connect(DEFAULT_SQL_HOST, "sunn", DEFAULT_SQL_USER, null);
 			sql.query("SELECT id, nick, count FROM "+WORDS_DB+" WHERE nick='"+sender+"';");
 			
 			
@@ -44,16 +47,39 @@ public class WordCount implements GrouphugModule {
 	}
 	
 	public void specialTrigger(String channel, String sender, String login, String hostname, String message){
-		//maybe later some time
+		addWords(sender, message);
 	}
 	public void trigger(String channel, String sender, String login, String hostname, String message){
-		//laterz
+        SQL sql = new SQL();
+		String nick = message.substring(10, message.length());
+        try{
+			sql.connect(DEFAULT_SQL_HOST, "sunn", DEFAULT_SQL_USER, null);
+			sql.query("SELECT id, nick, count FROM "+WORDS_DB+" WHERE nick='"+nick+"';");
+
+
+			if(!sql.getNext()) {
+				bot.sendMessage(nick + "doesn't have any words counted.", false);
+			}else{
+				Object[] values = sql.getValueList();
+				bot.sendMessage(nick + "has said "+values[2], false);
+			}
+
+		}catch(SQLException e) {
+            System.err.println(" > SQL Exception: "+e.getMessage()+"\n"+e.getCause());
+            bot.sendMessage("Sorry, an SQL error occured.", false);
+		}finally {
+            sql.disconnect();
+        }
+
 	}
 	public String helpMainTrigger(String channel, String sender, String login, String hostname, String message){
-        return "words";
+        if(message.equals(TRIGGER_HELP)) {
+            bot.sendNotice(sender, "Counts the number of words a person has said");
+            bot.sendNotice(sender, "To check how many words someone has said, use " +Grouphug.MAIN_TRIGGER + TRIGGER + "<nick>" );
+        }
     }
 	public boolean helpSpecialTrigger(String channel, String sender, String login, String hostname, String message){
-		return true;
+		return false;
 	}
 			
 }
