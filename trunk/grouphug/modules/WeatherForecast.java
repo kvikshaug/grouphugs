a0aa06ebd6c2f7d35563306203c2ea2bf5797c4c
murray <murray@c6f41995-d01f-4a11-924a-98aa25776321>
2008-12-11 06:11:26 +0000

    Testing module reload
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@253 c6f41995-d01f-4a11-924a-98aa25776321

--- trunk/grouphug/modules/WeatherForecast.java
+++ trunk/grouphug/modules/WeatherForecast.java
@@ -33,7 +33,7 @@ public class WeatherForecast implements GrouphugModule {
 
     public void trigger(String channel, String sender, String login, String hostname, String message) {
         if(message.equals("test")) {
-            Grouphug.getInstance().sendMessage("ja? er det håp?", false);
+            Grouphug.getInstance().sendMessage("jepp, det er det .p", false);
         }
         if(!message.startsWith(WeatherForecast.TRIGGER))
             return;
