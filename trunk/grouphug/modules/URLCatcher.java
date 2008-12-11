c44488bd4ae3e74dadbe71b181382fac356c333c
sunn <sunn@c6f41995-d01f-4a11-924a-98aa25776321>
2008-12-11 02:12:43 +0000

    Removed one sendMessage.
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@241 c6f41995-d01f-4a11-924a-98aa25776321

--- trunk/grouphug/modules/URLCatcher.java
+++ trunk/grouphug/modules/URLCatcher.java
@@ -67,7 +67,6 @@ public class URLCatcher implements GrouphugModule
         {
             String title = getHTMLTitle(url);
             if (title != null)
-                bot.sendMessage("[ " + url + " ] is " + title, false);
                 bot.sendMessage("Title: " + title + " :: " + url, false);
         }
     }
