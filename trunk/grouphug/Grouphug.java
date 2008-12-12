6c5da322f4231c2a56b7d5bf7c4bc0d2d45c2b9f
murray <murray@c6f41995-d01f-4a11-924a-98aa25776321>
2008-12-12 06:29:09 +0000

    Found the bug
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@269 c6f41995-d01f-4a11-924a-98aa25776321

--- trunk/grouphug/Grouphug.java
+++ trunk/grouphug/Grouphug.java
@@ -440,8 +440,8 @@ public class Grouphug extends PircBot {
         try {
             logfile.createNewFile();
             stdOut = new PrintStream(new BufferedOutputStream(new FileOutputStream(logfile)));
-            //System.setOut(stdOut);
-            //System.setErr(stdOut);
+            System.setOut(stdOut);
+            System.setErr(stdOut);
         } catch(IOException e) {
             System.err.println("Fatal error: Unable to load or create logfile \""+logfile.toString()+"\" in default dir.");
             e.printStackTrace();
