bf3580e56baf3ed46860cdc4b52828b96d8371e0
krashk <krashk@c6f41995-d01f-4a11-924a-98aa25776321>
2009-02-25 00:04:43 +0000

    Let's try a new seen module...
    git-svn-id: svn://hinux.hin.no/home/DT2006/murray/svn/gh@311 c6f41995-d01f-4a11-924a-98aa25776321

---
+++ trunk/grouphug/modules/Seen.java
@@ -0,0 +1,97 @@
+package grouphug.modules;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.util.Date;
+
+import grouphug.Grouphug;
+import grouphug.GrouphugModule;
+import grouphug.SQL;
+import grouphug.util.PasswordManager;
+
+public class Seen implements GrouphugModule {
+
+    private static final String DEFAULT_SQL_HOST = "127.0.0.1";
+    private static final String DEFAULT_SQL_USER = "gh";
+    private static final String TRIGGER_HELP = "seen";
+    private static final String TRIGGER = "seen ";
+    
+    private static final String SEEN_DB = "gh_seen";
+	
+	
+	
+	
+	
+	
+	@Override
+	public String helpMainTrigger(String channel, String sender, String login, String hostname, String message) {
+		return TRIGGER_HELP;
+	}
+
+	@Override
+	public String helpSpecialTrigger(String channel, String sender, String login, String hostname, String message) {
+		if(message.equals(TRIGGER_HELP)) {
+            return "Seen: When someone last said something in this channel\n" +
+                   "  " + Grouphug.MAIN_TRIGGER + TRIGGER + "<nick>\n";
+        }
+        return null;
+    }
+
+	@Override
+	public void specialTrigger(String channel, String sender, String login, String hostname, String message) {
+		SQL sql = new SQL();
+		try{
+			sql.connect(DEFAULT_SQL_HOST, "sunn", DEFAULT_SQL_USER, PasswordManager.getHinuxPass());
+			sql.query("SELECT id, nick FROM "+SEEN_DB+" WHERE nick='"+sender+"';");
+						
+			if(!sql.getNext()) {
+				sql.query("INSERT INTO "+SEEN_DB+" (nick, date) VALUES ('"+sender+"', now());");
+			}else{
+				Object[] values = sql.getValueList();
+				sql.query("UPDATE "+SEEN_DB+" SET date= now() WHERE id='"+values[0]+"';");
+			}
+
+		}catch(SQLException e) {
+            System.err.println(" > SQL Exception: "+e.getMessage()+"\n"+e.getCause());
+            Grouphug.getInstance().sendMessage("Sorry, an SQL error occured.", false);
+		}finally {
+            sql.disconnect();
+        }
+		
+	}
+
+	@Override
+	public void trigger(String channel, String sender, String login, String hostname, String message) {
+		if(message.startsWith(TRIGGER)){
+			print(message);
+		}
+		
+	}
+	
+	private void print(String message){
+		SQL sql = new SQL();
+        String nick = message.substring(TRIGGER.length());
+        try{
+            sql.connect(DEFAULT_SQL_HOST, "sunn", DEFAULT_SQL_USER, PasswordManager.getHinuxPass());
+            sql.query("SELECT id, nick, date FROM "+SEEN_DB+" WHERE nick='"+nick+"';");
+
+
+            if(!sql.getNext()) {
+                Grouphug.getInstance().sendMessage(nick + " hasn't said anything yet.", false);
+            }else{
+                Object[] values = sql.getValueList();
+                Date last = new Date(((Timestamp)values[2]).getTime());
+
+                Grouphug.getInstance().sendMessage(nick + " last uttered shit " +last, false);
+            }
+
+        }catch(SQLException e) {
+            System.err.println(" > SQL Exception: "+e.getMessage()+"\n"+e.getCause());
+            Grouphug.getInstance().sendMessage("Sorry, an SQL error occured.", false);
+        }finally {
+            sql.disconnect();
+        }
+    }
+
+}
