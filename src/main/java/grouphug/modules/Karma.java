b2cf4aaec8983d54578665bf069a2c86bb491512
Alex Kvikshaug <alex.kvikshaug@gmail.com>
2009-10-05 17:07:20 +0200

    Just removing the encoding "fixing" until we manage to understand what we're really doing

--- src/main/java/grouphug/modules/Karma.java
+++ src/main/java/grouphug/modules/Karma.java
@@ -80,7 +80,7 @@ public class Karma implements GrouphugModule {
     }
 
     private String norwegianCharsToHtmlEntities(String str) {
-        str = Grouphug.fixEncoding(str);
+        //str = Grouphug.fixEncoding(str);
         str = str.replace("æ", "&aelig;");
         str = str.replace("ø", "&oslash;");
         str = str.replace("å", "&aring;");
