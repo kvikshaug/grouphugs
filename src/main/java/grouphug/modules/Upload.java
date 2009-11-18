0ebfd25c0b4d12b7c356de85b3f9328a423b24d1
Ali Kaafarani <kaafarani.ali@gmail.com>
2009-11-18 19:45:30 +0100

    Rewrite upload module for the new server

--- src/main/java/grouphug/modules/Upload.java
+++ src/main/java/grouphug/modules/Upload.java
@@ -1,61 +1,57 @@
 package grouphug.modules;
 
-
 import grouphug.Grouphug;
 import grouphug.ModuleHandler;
+import grouphug.exceptions.SQLUnavailableException;
 import grouphug.listeners.TriggerListener;
-import grouphug.util.FileDataDownload;
+import grouphug.util.SQL;
 import grouphug.util.SQLHandler;
+import grouphug.util.Web;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Upload implements TriggerListener {
 
     private static final String TRIGGER_HELP = "upload";
     private static final String TRIGGER = "upload";
-    private static final String UPLOAD_DB= "upload";
-    private static final String TRIGGER_KEYWORD = "keyword ";
-    private static final String IMAGE_DIRECTORY = "img";
+    private static final String UPLOAD_DB= "uploads";
+    //private static final String TRIGGER_KEYWORD = "keyword ";
+    private static final String DESTINATION_DIR = "/var/www/gh/public/images/up/";
 
     private SQLHandler sqlHandler;
 
-    // TODO fix for the new server
-
     public Upload(ModuleHandler moduleHandler) {
-        System.err.println("Upload module has not been configured for its new server yet.");
-        /*
         try {
             sqlHandler = SQLHandler.getSQLHandler();
             // moduleHandler.addMessageListener(this);
             moduleHandler.addTriggerListener(TRIGGER, this);
             moduleHandler.registerHelp(TRIGGER_HELP, " A module to upload pictures and other things to gh\n" +
-                    " To use the module type " +Grouphug.MAIN_TRIGGER + TRIGGER + "<url> <keyword>\n" +
-                    " To get links associated with a keyword type "+ Grouphug.MAIN_TRIGGER + TRIGGER_KEYWORD+"<keyword>");
-        } catch(ClassNotFoundException ex) {
-            System.err.println("Upload module startup error: SQL unavailable!");
+                    " To use the module type " +Grouphug.MAIN_TRIGGER + TRIGGER + " <url> <keyword>"//\n" +
+                    /*" To get links associated with a keyword type "+ Grouphug.MAIN_TRIGGER + TRIGGER_KEYWORD+"<keyword>"*/);
+        } catch(SQLUnavailableException ex) {
+            System.err.println("Upload module startup error: SQL is unavailable!");
         }
-        */
     }
 
 
-    public void onMessage(String channel, String sender, String login, String hostname, String message) {
-        //TODO upload pictures and the like
-        //Only does something when asked
-    }
+    /*public void onMessage(String channel, String sender, String login, String hostname, String message) {
+        TODO always upload pictures when posted
+    }*/
 
     public void onTrigger(String channel, String sender, String login, String hostname, String message) {
-        if(message.startsWith(TRIGGER)){
-            insert(message.substring(TRIGGER.length()), sender);
-        }
-        else if(message.startsWith(TRIGGER_KEYWORD))
+        //if(message.startsWith(TRIGGER)) {
+            insert(message, sender);
+        /*} else if(message.startsWith(TRIGGER_KEYWORD)) {
             showUploads(message.substring(TRIGGER_KEYWORD.length()));
-
+        }*/
     }
 
-    private void showUploads(String keyword) {
-        try{
+    /*private void showUploads(String keyword) {
+        try {
             ArrayList<Object[]> rows = sqlHandler.select("SELECT url, nick FROM "+ UPLOAD_DB+" WHERE keyword='"+keyword+"';");
 
             if(rows.size() == 0) {
@@ -70,45 +66,65 @@ public class Upload implements TriggerListener {
             System.err.println(" > SQL Exception: "+e.getMessage()+"\n"+e.getCause());
             Grouphug.getInstance().sendMessage("Sorry, an SQL error occured.");
         }
-    }
+    }*/
 
     private void insert(String message, String sender) {
         //Split the message into URL and keyword, URL first
         String[] parts = message.split(" ");
-        String filename = parts[0].substring(parts[0].lastIndexOf('/')+1);
+        int lastSlashIndex = parts[0].lastIndexOf('/');
+        if(lastSlashIndex == -1) {
+            Grouphug.getInstance().sendMessage("That's not a valid URL now, is it?");
+            return;
+        }
+        if(parts.length <= 1) {
+            Grouphug.getInstance().sendMessage("Please provide both a valid URL and keyword.");
+            return;
+        }
+        int nextQuestionmarkIndex = parts[0].indexOf('?', lastSlashIndex);
+        String filename;
+        if(nextQuestionmarkIndex != -1) {
+            filename = parts[0].substring(lastSlashIndex+1, nextQuestionmarkIndex);
+        } else {
+            filename = parts[0].substring(lastSlashIndex+1);
+        }
 
-        try{
+        // if the file exists, add a number to the front of it
+        if(new File(DESTINATION_DIR + filename).exists()) {
+            int number = 1;
+            while(new File(DESTINATION_DIR + number + filename).exists()) {
+                number++;
+            }
+            filename = number + filename;
+        }
+
+        try {
+            Web.downloadFile(parts[0], filename, DESTINATION_DIR);
             ArrayList<String> params = new ArrayList<String>();
-            params.add(filename);
             params.add(parts[1]);
             params.add(sender);
-            sqlHandler.insert("INSERT INTO "+UPLOAD_DB+" (url, keyword, nick) VALUES (?,?,?);", params);
-
-            /* The following is (was) a good faith-attempt to do SQL properly:
-
-            PreparedStatement statement = sql.getConnection().prepareStatement("INSERT INTO "+UPLOAD_DB+" (url, keyword, nick) VALUES (?,?,?);");
-            statement.setString(1, filename);
-            statement.setString(2, parts[1]);
-            statement.setString(3, sender);
-            sql.executePreparedUpdate(statement);
-
-            */
-
-		} catch(SQLException e) {
-            System.err.println(" > SQL Exception: "+e.getMessage()+"\n"+e.getCause());
-            Grouphug.getInstance().sendMessage("Sorry, an SQL error occured.");
+            params.add(filename);
+            params.add(SQL.dateToSQLDateTime(new Date()));
+            sqlHandler.insert("INSERT INTO "+UPLOAD_DB+" (keyword, nick, filename, date) VALUES (?,?,?,?);", params);
+        } catch (IOException e) {
+            System.err.println("Failed to copy the file to the local filesystem.");
+            Grouphug.getInstance().sendMessage("Why am I expected to be able to upload everything?");
+            e.printStackTrace();
+            return;
+        } catch(SQLException e) {
+            System.err.println("SQL Exception: "+e.getMessage()+"\n"+e.getCause());
+            e.printStackTrace();
+            Grouphug.getInstance().sendMessage("An SQL error occured, but the file was probably saved successfully " +
+                    "before that happened. Go check the logs and clean up my database, you fool.");
+            return;
         }
-        //Now we download the file, at least we hope so
-        FileDataDownload.fileDownload(parts[0], IMAGE_DIRECTORY);
 
         // And fix the permissions
-        try {
+        /*try {
             Runtime.getRuntime().exec("chmod o+r "+IMAGE_DIRECTORY+filename);
         } catch(IOException ex) {
             System.err.println(ex);
-        }
+        }*/
         // Prints the URL to the uploaded file to the channel
-        Grouphug.getInstance().sendMessage("http://gh.kvikshaug.no/?/"+filename,false);
-
+        Grouphug.getInstance().sendMessage("Saved to http://gh.kvikshaug.no/images/up/" + filename);
     }
 }
