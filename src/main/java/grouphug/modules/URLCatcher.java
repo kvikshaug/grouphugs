3a3369435e26efbe4690aba90fc38c97a96865b4
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2009-11-27 23:44:53 +0100

    did some refactoring
    the guessEncoding method is now available in util.Web

--- src/main/java/grouphug/modules/URLCatcher.java
+++ src/main/java/grouphug/modules/URLCatcher.java
@@ -29,7 +29,6 @@ public class URLCatcher implements MessageListener {
     private static final String[] URI_SCHEMES = new String[]{"http://", "https://"};
     private static final String HELP_TRIGGER = "urlcatcher";
     private static final int TITLE_MAX_LENGTH = 100;
-
     public URLCatcher(ModuleHandler moduleHandler) {
         moduleHandler.addMessageListener(this);
         moduleHandler.registerHelp(HELP_TRIGGER, "URLCatcher tries to catch http:// or https:// URLs in messages to the channel, tries " +
@@ -41,41 +40,75 @@ public class URLCatcher implements MessageListener {
     @Override
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
         for(URL url : findAllUrls(message)) {
-            String title = null;
-            try {
-                title = getHTMLTitle(url);
-            } catch (IOException e) {
-                System.err.println("[URLCatcher] caught IOException while parsing HTML");
-            } catch (JDOMException e) {
-                System.err.println("[URLCatcher] caught JDOMException while parsing HTML");
-            }
+            String title = getTitle(url);
 
             if (title != null && title.length() > 0) {
                 if(title.length() > TITLE_MAX_LENGTH) {
                     title = title.substring(0, TITLE_MAX_LENGTH - 6); // minus the 6 ' (...)'-chars
                     title = title.concat(" (...)");
                 }
-                
+
                 Grouphug.getInstance().sendMessage("Title: " + title);
             }
         }
     }
 
+    private String getTitle(URL url) {
+        String encoding = Web.guessEncoding(url);
+        Reader r = setUpInputReader(url, encoding);
+
+        String title = null;
+        try {
+            title =  getHTMLTitle(r);
+        } catch (IOException e) {
+            System.err.println("[URLCatcher] caught IOException while parsing HTML");
+        } catch (JDOMException e) {
+            System.err.println("[URLCatcher] caught JDOMException while parsing HTML");
+        }
+
+        // do some tidying up
+        if (title != null) {
+            // strip trailing whitespace
+            title = title.trim();
+
+            // strip newlines
+            title = title.replaceAll("\n", " ");
+            title = title.replaceAll("\r\n", " ");
+
+            // strip tabs
+            title = title.replaceAll("\t", " ");
+        }
+
+        return title;
+    }
+
+    private Reader setUpInputReader(URL url, String encoding) {
+        InputStreamReader isr = null;
+        try {
+            isr = new InputStreamReader(url.openStream(), encoding);
+        } catch (IOException e) {
+            System.err.println("[URLCatcher]: caught IOException while initializing reader for parsing");
+        }
+        return isr;
+    }
+
     /**
      * Try to find the title of the html document that maybe is at url.
      *
-     * @param url the url that maybe points to a html document.
+     * @param r a stream of data from an uri
      * @return the title of the html document (if it's there, obviously).
      * @throws java.io.IOException if we run in to trouble somewhere.
      * @throws org.jdom.JDOMException if parsing fails
      */
-    private String getHTMLTitle(URL url) throws IOException, JDOMException {
+    private String getHTMLTitle(Reader r) throws IOException, JDOMException {
         SAXBuilder builder = new SAXBuilder("org.ccil.cowan.tagsoup.Parser"); // build a JDOM tree from the SAX stream provided by tagsoup
-        Document doc = builder.build(url);
+        Document doc = builder.build(r);
 
         XPath titlePath = XPath.newInstance("/h:html/h:head/h:title"); // find the <title> element using XPath
         titlePath.addNamespace("h","http://www.w3.org/1999/xhtml");
-        return ((Element)titlePath.selectSingleNode(doc)).getText(); // <title>return this</title>
+        String title = ((Element)titlePath.selectSingleNode(doc)).getText(); // <title>return this</title>
+
+        return title;
     }
 
     /**
@@ -97,4 +130,6 @@ public class URLCatcher implements MessageListener {
 
         return urls;
     }
+
+
 }
