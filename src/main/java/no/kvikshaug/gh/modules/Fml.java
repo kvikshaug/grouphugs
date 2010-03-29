7c755dcb2c6fc45950cefa0a7b6cc7eb0596a45f
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-03-29 05:10:24 +0200

    Add FML module

---
+++ src/main/java/no/kvikshaug/gh/modules/Fml.java
@@ -0,0 +1,46 @@
+package no.kvikshaug.gh.modules;
+
+import no.kvikshaug.gh.Grouphug;
+import no.kvikshaug.gh.ModuleHandler;
+import no.kvikshaug.gh.listeners.TriggerListener;
+import no.kvikshaug.gh.util.Web;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * A quickly written FML module
+ */
+public class Fml implements TriggerListener {
+
+    private Grouphug bot;
+
+    public Fml(ModuleHandler handler) {
+        bot = Grouphug.getInstance();
+        handler.registerHelp("fml", "Fuck my life\n!fml  -- show a fuck my life story from fmylife.com");
+        handler.addTriggerListener("fml", this);
+        System.out.println("FML module loaded.");
+    }
+
+    public void onTrigger(String channel, String sender, String login, String hostname, String message, String trigger) {
+        try {
+            Document doc = Web.getJDOMDocument(new URL("http://www.fmylife.com/random"));
+            XPath xpath = XPath.newInstance("//h:a[@class='fmllink']/parent::*");
+            xpath.addNamespace("h", "http://www.w3.org/1999/xhtml");
+            Element element = (Element)xpath.selectSingleNode(doc);
+            for(Object childElement : element.getChildren()) {
+                bot.sendMessage(((Element)childElement).getText());
+            }
+        } catch (IOException e) {
+            bot.sendMessage("Omg IOException.");
+            e.printStackTrace();
+        } catch (JDOMException e) {
+            bot.sendMessage("Omg omg JDOMException");
+            e.printStackTrace();
+        }
+    }
+}
