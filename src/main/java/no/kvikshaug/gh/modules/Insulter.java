57d39f5e3c8990ed8f9501ca05db3eee23805fa9
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-07-06 16:19:17 +0200

    Rewrite Insult module to use JDOM API

--- src/main/java/no/kvikshaug/gh/modules/Insulter.java
+++ src/main/java/no/kvikshaug/gh/modules/Insulter.java
@@ -4,17 +4,24 @@ import no.kvikshaug.gh.Grouphug;
 import no.kvikshaug.gh.ModuleHandler;
 import no.kvikshaug.gh.listeners.TriggerListener;
 import no.kvikshaug.gh.util.Web;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.xpath.XPath;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
 public class Insulter implements TriggerListener {
 
     private static final String TRIGGER = "insult";
     private static final String TRIGGER_HELP = "insult";
+    private static final String INSULT_URL = "http://www.randominsults.net/";
+
+    private Grouphug bot;
 
     public Insulter(ModuleHandler moduleHandler) {
+        bot = Grouphug.getInstance();
         moduleHandler.addTriggerListener(TRIGGER, this);
         moduleHandler.registerHelp(TRIGGER_HELP, "Insult someone you don't like:\n" +
                     "  "+Grouphug.MAIN_TRIGGER+TRIGGER +" <person>\n" +
@@ -28,42 +35,39 @@ public class Insulter implements TriggerListener {
         if(!message.equals("")) {
             insulted = message;
         }
-        String searchQ = "<td bordercolor=\"#FFFFFF\"><font face=\"Verdana\" size=\"4\"><strong><i>";
 
         try {
-            List<String> lines = Web.fetchHtmlLines(new URL("http://www.randominsults.net/"));
-            for(String line : lines) {
-                int insultStart = line.indexOf(searchQ);
-                if(insultStart == -1) {
-                    continue;
-                }
-                int insultEnd = line.indexOf('<', insultStart + searchQ.length());
-                if(insultEnd == -1) {
-                    continue;
-                }
-                String insult = line.substring(insultStart + searchQ.length(), insultEnd);
-
-                if(insulted != null) {
-                    Grouphug.getInstance().sendMessage(insulted+": "+insult);
-                } else {
-                    Grouphug.getInstance().sendMessage(insult);
-                }
-                return;
-            }
+            String insult = parseInsult();
             if(insulted != null) {
-                Grouphug.getInstance().sendMessage("Sorry, I was unable to parse randominsults.net because I was too " +
-                    "busy throwing up by " + insulted + "'s ghastly presence.");
+                bot.sendMessage(insulted + ": " + insult);
             } else {
-                Grouphug.getInstance().sendMessage("Sorry, I was unable to parse randominsults.net because I was too " +
-                        "busy throwing up by your ghastly presence.");
+                bot.sendMessage(insult);
             }
-        } catch(IOException ex) {
+        } catch(IOException e) {
             if(insulted != null) {
-                Grouphug.getInstance().sendMessage("Sorry, " + insulted + "'s ghastly presence made me throw up an IOException.");
+                bot.sendMessage("Sorry, " + insulted + "'s ghastly presence made me throw up an IOException.");
             } else {
-                Grouphug.getInstance().sendMessage("Sorry, your ghastly presence made me throw up an IOException.");
+                bot.sendMessage("Sorry, your ghastly presence made me throw up an IOException.");
             }
-            ex.printStackTrace();
+            e.printStackTrace();
+        } catch (JDOMException e) {
+            if(insulted != null) {
+                bot.sendMessage("Sorry, " + insulted + "'s ghastly presence made me throw up a JDOMException.");
+            } else {
+                bot.sendMessage("Sorry, your ghastly presence made me throw up a JDOMException.");
+            }
+            e.printStackTrace();
         }
+    }
+
+    private String parseInsult() throws IOException, JDOMException {
+        Document doc = Web.getJDOMDocument(new URL(INSULT_URL));
+        XPath insultPath = XPath.newInstance("//h:strong/h:i");
+        insultPath.addNamespace("h","http://www.w3.org/1999/xhtml");
+        Element insultElement = (Element)insultPath.selectSingleNode(doc);
+        if(insultElement == null) {
+            return "The insult generator is so appalled by your prescense that it didn't even supply an insult.";
+        }
+        return insultElement.getText();
     }
 }
