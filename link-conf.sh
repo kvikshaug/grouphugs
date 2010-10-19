05bc6a6c30d4c4c683c9176f485e3c0345b6263e
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2010-10-19 19:31:28 +0200

    force symlinks

--- link-conf.sh
+++ link-conf.sh
@@ -3,5 +3,5 @@ set -e
 pushd "$(dirname $0)" # make sure we're in the correct directory
 
 pushd target
-ln -s ../props.xml props.xml # symlink props.xml
+ln -sf ../props.xml props.xml # symlink props.xml
 popd
--- link-db.sh
+++ link-db.sh
@@ -3,5 +3,5 @@ set -e
 pushd "$(dirname $0)" # make sure we're in the correct directory
 
 pushd target
-ln -s ../grouphugs.db grouphugs.db # symlink sqlite database
+ln -sf ../grouphugs.db grouphugs.db # symlink sqlite database
 popd
