bf051f90bc8c30efb0d7ffb485b1b3449ce0c6d5
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2010-10-19 19:31:42 +0200

    make links on startup

--- restart-bot.sh
+++ restart-bot.sh
@@ -5,6 +5,9 @@ then
     kill "$(cat /tmp/gh.pid)" # kill bot
 fi
 
+./link-conf.sh
+./link-db.sh
+
 pushd "$(dirname $0)/target" # make sure we're in the correct directory
 java -jar gh-1.0.0.jar > gh.log 2>&1 & # restart bot
 echo "$!" > /tmp/gh.pid # save pid
