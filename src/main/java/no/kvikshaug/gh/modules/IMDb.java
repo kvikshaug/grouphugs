094c94ff6e1fc629e2da126c18b27642b4c3060e
magnuf <magnus.fjellatgmail.com>
2010-10-31 01:44:14 +0200

    Now using the Web.fetchTitle to get the title of the movie. Also changed where to get the plot, to see if this works.

--- src/main/java/no/kvikshaug/gh/modules/IMDb.java
+++ src/main/java/no/kvikshaug/gh/modules/IMDb.java
@@ -19,86 +19,80 @@ import org.jdom.xpath.XPath;
 
 public class IMDb implements TriggerListener {
 
-    private static final String TRIGGER = "imdb";
-    private static final String TRIGGER_HELP = "imdb";
+	private static final String TRIGGER = "imdb";
+	private static final String TRIGGER_HELP = "imdb";
 
-    public IMDb(ModuleHandler moduleHandler) {
-        moduleHandler.addTriggerListener(TRIGGER, this);
-        moduleHandler.registerHelp(TRIGGER_HELP, "IMDb: Show IMDb info for a movie\n" +
-                   "  "+Grouphug.MAIN_TRIGGER+TRIGGER +"<movie name>");
-        System.out.println("IMDb module loaded.");
-    }
+	public IMDb(ModuleHandler moduleHandler) {
+		moduleHandler.addTriggerListener(TRIGGER, this);
+		moduleHandler.registerHelp(TRIGGER_HELP, "IMDb: Show IMDb info for a movie\n" +
+				"  "+Grouphug.MAIN_TRIGGER+TRIGGER +"<movie name>");
+		System.out.println("IMDb module loaded.");
+	}
 
 
-    public void onTrigger(String channel, String sender, String login, String hostname, String message, String trigger) {
-        URL imdbURL;
-        try {
-            imdbURL = Web.googleSearch(message+"+site:www.imdb.com").get(0);
-        } catch(IndexOutOfBoundsException ex) {
-            Grouphug.getInstance().sendMessage("Sorry, I didn't find "+message+" on IMDb.");
-            return;
-        } catch(IOException e) {
-            Grouphug.getInstance().sendMessage("But I don't want to. (IOException)");
-            return;
-        } catch (JDOMException e) {
-            Grouphug.getInstance().sendMessage("I seem to have thrown a JDOMException. Woopsie!");
-            return;
-        }
+	public void onTrigger(String channel, String sender, String login, String hostname, String message, String trigger) {
+		URL imdbURL;
+		try {
+			imdbURL = Web.googleSearch(message+"+site:www.imdb.com").get(0);
+		} catch(IndexOutOfBoundsException ex) {
+			Grouphug.getInstance().sendMessage("Sorry, I didn't find "+message+" on IMDb.");
+			return;
+		} catch(IOException e) {
+			Grouphug.getInstance().sendMessage("But I don't want to. (IOException)");
+			return;
+		} catch (JDOMException e) {
+			Grouphug.getInstance().sendMessage("I seem to have thrown a JDOMException. Woopsie!");
+			return;
+		}
 
-        
-        String title = "";
-        String score = "";
-        String plot = "";
-        Document doc;
+
+		String title = "";
+		String score = "";
+		String plot = "";
+
+		Document doc;
 		try {
 			doc = Web.getJDOMDocument(imdbURL);
-        
-        // find the title of the movie element using XPath
-        XPath titlePath = XPath.newInstance("//h:h1[@class='header']");
-        titlePath.addNamespace("h","http://www.w3.org/1999/xhtml");
 
-        Element titleElement = (Element)titlePath.selectSingleNode(doc);
-        if(titleElement == null) {
-            throw new NoTitleException("No title element in DOM");
-        }
-        title = titleElement.getText();
-        
-        // find the score of the movie element using XPath
-        XPath scorePath = XPath.newInstance("//h:span[@id='star-bar-user-rate']/h:b");
-        scorePath.addNamespace("h","http://www.w3.org/1999/xhtml");
-        
-        Element scoreElement = (Element)scorePath.selectSingleNode(doc);
-        if(scoreElement == null) {
-            throw new JDOMException("No score element in DOM");
-        }
-        score = scoreElement.getText();
-        
-        
-        // find the score of the movie element using XPath
-        XPath plotPath = XPath.newInstance("//h:table[@id='title-overview-widget-layout']/h:tbody/h:tr/h:td[2]/h:p[2]");
-        plotPath.addNamespace("h","http://www.w3.org/1999/xhtml");
-        
-        Element plotElement = (Element)plotPath.selectSingleNode(doc);
-        if(plotElement == null) {
-            throw new JDOMException("No plot element in DOM");
-        }
-        plot = plotElement.getText();
-        
-        
-        } catch (IOException e) {
-        	e.printStackTrace();
-        	System.out.println(e.getMessage());
-        } catch (JDOMException e) {
-        	e.printStackTrace();
-        	System.out.println(e.getMessage());
-        } catch (NoTitleException e) {
+			title = Web.fetchTitle(imdbURL);
+			title = title.substring(0, title.length()-7); //To remove the  - IMDb part of the title
+
+			// find the score of the movie element using XPath
+			XPath scorePath = XPath.newInstance("//h:span[@id='star-bar-user-rate']/h:b");
+			scorePath.addNamespace("h","http://www.w3.org/1999/xhtml");
+
+			Element scoreElement = (Element)scorePath.selectSingleNode(doc);
+			if(scoreElement == null) {
+				throw new JDOMException("No score element in DOM");
+			}
+			score = scoreElement.getText();
+
+
+			// find the score of the movie element using XPath
+			XPath plotPath = XPath.newInstance("//h:table[@id='title-overview-widget-layout']/h:tbody/h:tr/h:td[1]/h:p[1]");
+			plotPath.addNamespace("h","http://www.w3.org/1999/xhtml");
+
+			Element plotElement = (Element)plotPath.selectSingleNode(doc);
+			if(plotElement == null) {
+				throw new JDOMException("No plot element in DOM");
+			}
+			plot = plotElement.getText();
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		} catch (JDOMException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		} catch (NoTitleException e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
-        try {
-            Grouphug.getInstance().sendMessage(title+"\n"+plot+"\n"+"\n"+score+"/10\n"+imdbURL.toString());
-        } catch(NullPointerException ex) {
-            Grouphug.getInstance().sendMessage("The IMDb site layout may have changed, I was unable to parse it.");
-        }
-    }
+		try {
+			Grouphug.getInstance().sendMessage(title+"\n"+plot+"\n"+"\n"+score+"/10\n"+imdbURL.toString());
+		} catch(NullPointerException ex) {
+			Grouphug.getInstance().sendMessage("The IMDb site layout may have changed, I was unable to parse it.");
+		}
+	}
 }
