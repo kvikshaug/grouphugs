e17acea666fd1d439476336630aa6d0e3681aaca
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-11-23 17:51:26 +0100

    Add UserMask class in Operator

--- src/main/scala/no/kvikshaug/gh/modules/Operator.scala
+++ src/main/scala/no/kvikshaug/gh/modules/Operator.scala
@@ -8,5 +8,12 @@ import no.kvikshaug.gh.listeners.{JoinListener, NickChangeListener}
 
 import org.jibble.pircbot.User
 
+case class UserMask(nick: String, login: String, hostname: String) {
+  override def equals(other: Any) = other match {
+    case that: UserMask => nick == that.nick
+    case _ => false
+  }
+}
+
 class Operator(handler: ModuleHandler) extends JoinListener with NickChangeListener
 
