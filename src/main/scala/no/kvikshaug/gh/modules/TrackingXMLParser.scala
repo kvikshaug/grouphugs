fada964bff06bd39740647f4ae81f539c944c979
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-11-23 13:13:12 +0100

    Return appropriate values when track returns 404

--- src/main/scala/no/kvikshaug/gh/modules/TrackingXMLParser.scala
+++ src/main/scala/no/kvikshaug/gh/modules/TrackingXMLParser.scala
@@ -19,26 +19,37 @@ object TrackingXMLParser {
   @throws(classOf[SQLException])
   // returns a tuple of ("Has the package changed?", "The number of packages in this item")
   def track(item: Package): Tuple2[Boolean, Int] = {
-    // load the xml
-    val root = XML.load(Web.prepareEncodedBufferedReader(
-      new URL("http://sporing.posten.no/sporing.xml?q=" + item.id)))
+    var newStatus = ""
+    var newStatusCode = ""
+    var packageCount = 0
 
-    val e = ((root \\ "PackageSet" \ "Package")(0) \ "EventSet" \ "Event")(0)
-    val status = (e \ "Description").text.replaceAll("<.*?>", "") + " " +
-                 (e \ "PostalCode").text.replaceAll("<.*?>", "")  + " " +
-                 (e \ "City").text.replaceAll("<.*?>", "") + " " +
-                 (e \ "OccuredAtDisplayTime").text.replaceAll("<.*?>", "") + " " +
-                 (e \ "OccuredAtDisplayDate").text.replaceAll("<.*?>", "")
-    val statusCode = (e \ "Status").text.replaceAll("<.*?>", "")
-    val packageCount = (root \\ "PackageSet" \ "Package").size
+    try {
+      // load the xml
+      val root = XML.load(Web.prepareEncodedBufferedReader(
+        new URL("http://sporing.posten.no/sporing.xml?q=" + item.id)))
+
+      val e = ((root \\ "PackageSet" \ "Package")(0) \ "EventSet" \ "Event")(0)
+      newStatus = (e \ "Description").text.replaceAll("<.*?>", "") + " " +
+                  (e \ "PostalCode").text.replaceAll("<.*?>", "")  + " " +
+                  (e \ "City").text.replaceAll("<.*?>", "") + " " +
+                  (e \ "OccuredAtDisplayTime").text.replaceAll("<.*?>", "") + " " +
+                  (e \ "OccuredAtDisplayDate").text.replaceAll("<.*?>", "")
+      newStatusCode = (e \ "Status").text.replaceAll("<.*?>", "")
+      packageCount = (root \\ "PackageSet" \ "Package").size
+    } catch {
+      // thrown by the API when the item ID isn't found at posten (404)
+      case e: FileNotFoundException =>
+        newStatus = "Not found (but it might appear soon if it was recently added)"
+        newStatusCode = "NO_PACKAGES"
+    }
 
     var changed = false
-    if(status != item.status || statusCode != item.statusCode) {
+    if(newStatus != item.status || newStatusCode != item.statusCode) {
       changed = true
     }
-    item.status = status
-    item.statusCode = statusCode
-    sqlHandler.update("update " + dbName + " set status='" + status + "', statusCode='" + statusCode + "' where trackingId='" + item.id + "'")
+    item.status = newStatus
+    item.statusCode = newStatusCode
+    sqlHandler.update("update " + dbName + " set status='" + newStatus + "', statusCode='" + newStatusCode + "' where trackingId='" + item.id + "'")
 
     (changed, packageCount)
   }
