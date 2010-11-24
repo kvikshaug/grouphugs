a41ed18217035e164404895c5708a1eb5f06b5dc
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-11-24 16:25:13 +0100

    Remove unnecessary import

--- src/main/scala/no/kvikshaug/gh/modules/Operator.scala
+++ src/main/scala/no/kvikshaug/gh/modules/Operator.scala
@@ -1,12 +1,9 @@
 package no.kvikshaug.gh.modules
 
-import java.io.{File, IOException}
 import scala.xml._
 
 import no.kvikshaug.gh.{Grouphug, ModuleHandler}
 import no.kvikshaug.gh.listeners.{JoinListener, NickChangeListener}
-
-import org.jibble.pircbot.User
 
 case class UserMask(nick: String, login: String, hostname: String) {
   override def equals(other: Any) = other match {
