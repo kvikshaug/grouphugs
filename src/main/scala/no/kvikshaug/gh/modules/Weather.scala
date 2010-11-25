efe96a2658e97fdee4b4095b2c7d8a479dcb8e4d
Ali Kaafarani <kaafarani.ali@gmail.com>
2010-11-25 15:55:32 +0100

    Reformat linebreaks

--- src/main/scala/no/kvikshaug/gh/modules/Weather.scala
+++ src/main/scala/no/kvikshaug/gh/modules/Weather.scala
@@ -46,10 +46,10 @@ class Weather(val handler: ModuleHandler) extends TriggerListener {
     // current conditions
     if(input._2) {
       bot.sendMessageChannel(channel, "Currently: " +
-                  (root \ "current_conditions" \ "temp_c" \ "@data").text + "°C, " +
-                  (root \ "current_conditions" \ "condition" \ "@data").text + ". " +
-                  (root \ "current_conditions" \ "humidity" \ "@data").text + ". " +
-                  (root \ "current_conditions" \ "wind_condition" \ "@data").text)
+        (root \ "current_conditions" \ "temp_c" \ "@data").text + "°C, " +
+        (root \ "current_conditions" \ "condition" \ "@data").text + ". " +
+        (root \ "current_conditions" \ "humidity" \ "@data").text + ". " +
+        (root \ "current_conditions" \ "wind_condition" \ "@data").text)
     }
 
     // forecast
