6fe9f50dcfc57623e0d9092db9734d52e10519c0
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2011-03-13 16:29:07 +0100

    update scripts

--- git-pull-compile-link-restart.sh
+++ git-pull-compile-link-restart.sh
@@ -3,7 +3,7 @@ set -e
 pushd "$(dirname $0)" # make sure we're in the correct directory
 
 git pull "$@" # pull changes from a git remote
-buildr clean compile package # recompile, package
+mvn clean compile package # recompile, package
 
 ./link-db.sh
 ./link-conf.sh
--- restart-bot.sh
+++ restart-bot.sh
@@ -9,6 +9,6 @@ fi
 ./link-db.sh
 
 pushd "$(dirname $0)/target" # make sure we're in the correct directory
-java -jar gh-1.0.0.jar > gh.log 2>&1 & # restart bot
+java -jar grouphug-1.0-SNAPSHOT.jar > gh.log 2>&1 & # restart bot
 echo "$!" > /tmp/gh.pid # save pid
 popd
