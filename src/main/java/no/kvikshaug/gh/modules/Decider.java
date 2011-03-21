2c4e6057cb6994d248eba80ee5b2827bc7d1d021
magnuf <magnus.fjellatgmail.com>
2011-03-22 00:53:22 +0100

    Changed decider delimiter from space to |

--- src/main/java/no/kvikshaug/gh/modules/Decider.java
+++ src/main/java/no/kvikshaug/gh/modules/Decider.java
@@ -15,13 +15,13 @@ public class Decider implements TriggerListener {
     public Decider(ModuleHandler moduleHandler) {
         moduleHandler.addTriggerListener(TRIGGER, this);
         moduleHandler.registerHelp(TRIGGER_HELP, "Decider: Helps you make tough decisions.\n" +
-                    "  "+Grouphug.MAIN_TRIGGER+TRIGGER+"<choice 1> <choice 2> <choice n...>\n" +
-                    "  "+Grouphug.MAIN_TRIGGER+TRIGGER+"homework wow");
+                    "  "+Grouphug.MAIN_TRIGGER+TRIGGER+"<choice 1> | <choice 2> | <choice n...>\n" +
+                    "  "+Grouphug.MAIN_TRIGGER+TRIGGER+"homework | wow | keep ircing");
         System.out.println("Decider module loaded.");
     }
 
     public void onTrigger(String channel, String sender, String login, String hostname, String message, String trigger) {
-        String[] choices = message.split(" ");
-        Grouphug.getInstance().msg(channel, "The roll of the dice picks: "+choices[random.nextInt(choices.length)]);
+        String[] choices = message.split("|");
+        Grouphug.getInstance().msg(channel, "The roll of the dice picks: "+choices[random.nextInt(choices.length)].trim());
     }
 }
