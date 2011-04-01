73ccbb92ecef30ef7911463e5027bd0f7ffda304
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-04-01 18:42:20 +0200

    Let the user specify max output linecount

--- src/main/scala/no/kvikshaug/gh/modules/Repl.scala
+++ src/main/scala/no/kvikshaug/gh/modules/Repl.scala
@@ -15,12 +15,25 @@ class Repl(val handler: ModuleHandler) extends TriggerListener {
 
   val bot = Grouphug.getInstance
   handler.addTriggerListener("repl", this)
-  handler.registerHelp("repl", "!repl <command> - Send a command to simplyscala.com for REPL output")
+  handler.registerHelp("repl", """!repl <command> - Send a command to simplyscala.com for REPL output
+!repl -l 1 <command> - Show only 1 line (the default is 3 lines)
+!repl -l a <command> - Show all lines""")
   println("Repl module loaded.")
+
+  val defaultLineCount = 3
 
   def onTrigger(channel: String, sender: String, login: String, hostname: String, message: String, trigger: String): Unit = {
     // run this asynchronous
-    listener ! Message(message, channel)
+    val m = Pattern.compile("-l ([0-9|a]+) (.*)").matcher(message)
+    if(m matches) {
+      if(m.group(1) == "a") {
+        listener ! Message(m.group(2), -1, channel)
+      } else {
+        listener ! Message(m.group(2), m.group(1).toInt, channel)
+      }
+    } else {
+      listener ! Message(message, defaultLineCount, channel)
+    }
   }
 
   val maxTries = 5
@@ -47,20 +60,7 @@ class Repl(val handler: ModuleHandler) extends TriggerListener {
           }
           val pattern = Pattern.compile("""(?s).*<pre class="code">.*</pre>.*<pre.*>(.*)</pre>.*""").matcher(line)
           if(pattern matches) {
-            def nIndex(h: String, n: String, s: Int, c: Int): Int = {
-              if(c == 0 || s == -1) {
-                s
-              } else {
-                nIndex(h, n, h.indexOf(n, s + 1), c - 1)
-              }
-            }
-            // max 3 lines
-            line = pattern.group(1)
-            val thirdLineBreak = nIndex(line, "\n", 0, 3)
-            if(thirdLineBreak != -1) {
-              line = line.substring(0, thirdLineBreak)
-            }
-            bot.msg(message.channel, line)
+            bot.msg(message.channel, maxLines(pattern.group(1), message.lineCount), true)
           } else {
             bot.msg(message.channel, "Whoops, looks like the expected output at simplyscala.com has changed. My regex didn't match.")
           }
@@ -79,6 +79,14 @@ class Repl(val handler: ModuleHandler) extends TriggerListener {
     }
     br.close
     builder.toString
+  }
+
+  def maxLines(str: String, max: Int): String = max match {
+    case 0 => ""
+    case _ => str.indexOf('\n', 1) match {
+      case -1 => str
+      case i  => str.substring(0, i) + maxLines(str.substring(i), max - 1)
+    }
   }
 }
 
