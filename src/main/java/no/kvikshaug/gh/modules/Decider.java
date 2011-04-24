5759e5c850dfbb35eb2af0eefc707ab37d97418c
magnuf <magnus.fjellatgmail.com>
2011-04-24 18:52:19 +0200

    Fixed some spaces in decider help text

--- src/main/java/no/kvikshaug/gh/modules/Decider.java
+++ src/main/java/no/kvikshaug/gh/modules/Decider.java
@@ -15,8 +15,8 @@ public class Decider implements TriggerListener {
     public Decider(ModuleHandler moduleHandler) {
         moduleHandler.addTriggerListener(TRIGGER, this);
         moduleHandler.registerHelp(TRIGGER_HELP, "Decider: Helps you make tough decisions.\n" +
-                    "  "+Grouphug.MAIN_TRIGGER+TRIGGER+"<choice 1> | <choice 2> | <choice n...>\n" +
-                    "  "+Grouphug.MAIN_TRIGGER+TRIGGER+"homework | wow | keep ircing");
+                    Grouphug.MAIN_TRIGGER+TRIGGER+" <choice 1> | <choice 2> | <choice n...>\n" +
+                    Grouphug.MAIN_TRIGGER+TRIGGER+" homework | wow | keep ircing");
         System.out.println("Decider module loaded.");
     }
 
