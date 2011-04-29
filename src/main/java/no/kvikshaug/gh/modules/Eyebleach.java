9526a59dd1eb1d51feb782c7347ff23c0bde30d7
magnuf <magnus.fjellatgmail.com>
2011-04-29 14:59:02 +0200

    Added Eyebleach module from sandsmark

---
+++ src/main/java/no/kvikshaug/gh/modules/Eyebleach.java
@@ -0,0 +1,27 @@
+package no.kvikshaug.gh.modules;
+
+import no.kvikshaug.gh.Grouphug;
+import no.kvikshaug.gh.ModuleHandler;
+import no.kvikshaug.gh.listeners.TriggerListener;
+
+public class Eyebleach implements TriggerListener {
+
+    private static final String TRIGGER = "eyebleach";
+    private static final String TRIGGER_HELP = "eyebleach";
+
+    public Eyebleach(ModuleHandler moduleHandler) {
+        moduleHandler.addTriggerListener(TRIGGER, this);
+        moduleHandler.registerHelp(TRIGGER_HELP, "Eyebleach: administers a dose of eyebleach.\n" +
+                    Grouphug.MAIN_TRIGGER+TRIGGER+ "\n" +
+                    Grouphug.MAIN_TRIGGER+TRIGGER+" <message>");
+        System.out.println("Eyebleach module loaded.");
+    }
+
+    public void onTrigger(String channel, String sender, String login, String hostname, String message, String trigger) {
+        String output = "http://eyebleach.com/eyebleach/eyebleach_";
+        output += String.format("%03d", Math.random() * 98 + 1);
+        output += ".jpg";
+
+        Grouphug.getInstance().msg(channel, output);
+    }
+}
