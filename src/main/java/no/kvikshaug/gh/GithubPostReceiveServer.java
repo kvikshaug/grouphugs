b3807dc6f85d98e2965bb14f28735ae660376c14
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2011-08-26 14:15:13 +0200

    Remove unused import.

--- src/main/java/no/kvikshaug/gh/GithubPostReceiveServer.java
+++ src/main/java/no/kvikshaug/gh/GithubPostReceiveServer.java
@@ -15,7 +15,6 @@ import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
 import java.net.URI;
-import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
