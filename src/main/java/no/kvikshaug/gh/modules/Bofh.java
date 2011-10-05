cb6288a41c8afb4569943c9b4775979e26cf0c92
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-05 18:39:22 +0200

    Rewrite Bofh for Worm

--- src/main/java/no/kvikshaug/gh/modules/Bofh.java
+++ src/main/java/no/kvikshaug/gh/modules/Bofh.java
@@ -4,10 +4,12 @@ import no.kvikshaug.gh.Grouphug;
 import no.kvikshaug.gh.ModuleHandler;
 import no.kvikshaug.gh.exceptions.SQLUnavailableException;
 import no.kvikshaug.gh.listeners.TriggerListener;
-import no.kvikshaug.gh.util.SQLHandler;
+import no.kvikshaug.gh.util.SQL;
 
-import java.sql.SQLException;
-import java.util.Arrays;
+import no.kvikshaug.worm.Worm;
+import no.kvikshaug.worm.JWorm;
+
+import java.util.List;
 
 /**
  * Need a quick excuse to shut a luser up? Look no further, the BOFH module will assist you.
@@ -22,69 +24,57 @@ public class Bofh implements TriggerListener {
     private static final String SPECIFIC_TRIGGER = "\\d+";
     public static final String HELP_TRIGGER = RANDOM_TRIGGER;
 
-    private SQLHandler sql;
-
     public Bofh(ModuleHandler moduleHandler) {
-        try {
-            sql = SQLHandler.getSQLHandler();
+        if(SQL.isAvailable()) {
             moduleHandler.addTriggerListener(RANDOM_TRIGGER, this);
             moduleHandler.registerHelp(HELP_TRIGGER, "BOFH - Fend off lusers with Bastard Operator From Hell excuses for their system \"problems\".\n" +
                     "Usage:\n" +
                     Grouphug.MAIN_TRIGGER + RANDOM_TRIGGER + " returns a random excuse.\n" +
                     Grouphug.MAIN_TRIGGER + RANDOM_TRIGGER + " " + SPECIFIC_TRIGGER + " returns an excuse by number. (\\d+ means any digit, 1-n times)");
-        } catch (SQLUnavailableException e) {
+        } else {
             System.err.println("BOFH module disabled: needs to load stored data from SQL.");
         }
     }
 
-    private String formatExcuse(String id, String excuse) {
-        return String.format("BOFH excuse #%s: %s", id, excuse);
-    }
-
     private String getExcuse(String number) {
-        String excuse = null;
-        try {
-            Object[] row = sql.selectSingle("SELECT id, excuse FROM bofh WHERE id='?'", Arrays.asList(number));
-            if (row != null) {
-                excuse = formatExcuse(((Integer)row[0]).toString(), (String)row[1]);
-            }
-        } catch (ClassCastException e) {
-            System.err.println("BOFH failed: Yell at the developers!");
-        } catch (SQLException e) {
+        List<BofhExcuse> excuses = JWorm.getWith(BofhExcuse.class, "where id='" + SQL.sanitize(number) + "'");
+        if(excuses.size() == 1) {
+            return excuses.get(0).getFormattedExcuse();
+        } else {
             return "No bofh excuse for that number. Try a lower number.";
         }
-        return excuse;
     }
 
     private String getRandomExcuse() {
-        String excuse = null;
-        try {
-
-            SQLHandler sql = SQLHandler.getSQLHandler();
-            Object[] row = sql.selectSingle("SELECT id, excuse FROM bofh ORDER BY RANDOM() LIMIT 1");
-            if (row != null) {
-                excuse = formatExcuse(((Integer)row[0]).toString(), (String)row[1]);
-            }
-        } catch (ClassCastException e) {
-            System.err.println("BOFH failed: Yell at the developers!");
-        } catch (SQLUnavailableException e) {
-            System.err.println("BOFH failed: SQL is unavailable!");
-        } catch (SQLException e) {
-            return "No bofh excuse for that number. Try a lower number.";
+        List<BofhExcuse> excuses = JWorm.getWith(BofhExcuse.class, " order by random() limit 1");
+        if(excuses.size() == 1) {
+            return excuses.get(0).getFormattedExcuse();
+        } else {
+            return "Random managed to pick an excuse that didn't exist.";
         }
-        return excuse;
     }
 
     public void onTrigger(String channel, String sender, String login, String hostname, String message, String trigger) {
-        String reply;
         if(message.equals("")) {
-            reply = getRandomExcuse();
+            Grouphug.getInstance().msg(channel, getRandomExcuse());
         } else {
-            reply = getExcuse(message);
+            Grouphug.getInstance().msg(channel, getExcuse(message));
+        }
+    }
+
+    public static class BofhExcuse extends Worm {
+        private String excuse;
+
+        public BofhExcuse(String excuse) {
+            this.excuse = excuse;
         }
 
-        if (reply != null) {
-            Grouphug.getInstance().msg(channel, reply);
+        public String getExcuse() {
+            return excuse;
+        }
+
+        public String getFormattedExcuse() {
+            return String.format("BOFH excuse #%s: %s", wormDbId().get(), getExcuse());
         }
     }
 }
