3babaff20fc24a759ba44c3353fa17884bb19f02
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-10-06 19:43:25 +0200

    Rewrite Vote for Worm

--- src/main/scala/no/kvikshaug/gh/modules/Vote.scala
+++ src/main/scala/no/kvikshaug/gh/modules/Vote.scala
@@ -2,25 +2,24 @@ package no.kvikshaug.gh.modules
 
 import no.kvikshaug.gh.listeners.TriggerListener
 import no.kvikshaug.gh.{Grouphug, ModuleHandler}
-import no.kvikshaug.gh.exceptions.SQLUnavailableException;
-import no.kvikshaug.gh.util.SQLHandler;
+import no.kvikshaug.gh.util.SQL;
+
+import no.kvikshaug.worm.Worm;
 
 import java.util.regex._
-import java.sql.SQLException
 
-import scala.collection.JavaConverters._
-
-case class VoteItem(id: String, creator: String, text: String, multi: Boolean, options: List[VoteOption]) {
+case class VoteItem(creator: String, text: String, multi: Boolean,
+                    options: List[VoteOption]) extends Worm {
   def resultCount = options.foldLeft(0)((c, o) => c + o.voters.size)
   override def equals(other: Any) = other match {
-    case that: VoteItem => id == that.id
-    case thatId: String => id == thatId
+    case that: VoteItem => wormDbId.get == that.wormDbId.get
+    case thatId: String => wormDbId.get.toString == thatId
     case _ => false
   }
 }
-case class VoteOption(var text: String, var voters: List[String]) {
+case class VoteOption(var text: String, var voters: List[String]) extends Worm {
   override def equals(other: Any) = other match {
-    case that: VoteOption => text == that.text
+    case that: VoteOption => text.toLowerCase == that.text.toLowerCase
     case thatText: String => text.toLowerCase == thatText.toLowerCase
     case _ => false
   }
@@ -43,51 +42,13 @@ class Vote(val handler: ModuleHandler) extends TriggerListener {
 !endvote <vote>        -> Deletes a vote and corresponding results
 Add -m to !startvote to allow multiple choices from one candidate""")
 
-  var sqlHandler: SQLHandler = null
-  val dbVotes = "votes"
-  val dbVoteOptions = "vote_options"
-  val dbVoteOptionVoters = "vote_option_voters"
+  // load all votes from db
   var items = List[VoteItem]()
-
-  // load all votes from db. this is so ugly i can't even look at it.
-  try {
-    sqlHandler = SQLHandler.getSQLHandler
-    // votes
-    items = sqlHandler.select(
-      "select id, creator, text, multi from " + dbVotes + ";"
-    ).asScala.map { voteRow =>
-      // vote options
-      val optionList = sqlHandler.select(
-        "select id, text from " + dbVoteOptions + " where voteId='" + voteRow(0).toString + "';"
-      ).asScala.map { voteOptionRow =>
-        // vote option voters
-        val voterList = sqlHandler.select(
-          "select nick from " + dbVoteOptionVoters + " where optionId='" + voteOptionRow(0).toString + "';"
-        ).asScala.map { voteOptionVoterRow =>
-          voteOptionVoterRow(0).asInstanceOf[String]
-        }.toList;
-        VoteOption(voteOptionRow(1).asInstanceOf[String], voterList)
-      }.toList;
-    VoteItem(voteRow(0).toString, voteRow(1).asInstanceOf[String],
-             voteRow(2).asInstanceOf[String], voteRow(3).asInstanceOf[String].toBoolean,
-             optionList)
-    }.toList
-  } catch {
-    case e: SQLUnavailableException => println("Vote module warning: Existing votes couldn't be loaded because SQL is unavailable.")
-    case e: SQLException => println("Vote module warning: Existing votes couldn't be loaded because of an SQLException.")
-      e.printStackTrace();
-  }
-
-  val random = new java.util.Random
-  val idDigits = 4
-  def randomId = {
-    def generate = random.nextInt(9000) + 1000 // remember to change idDigits if changing these!
-    var id = generate
-    // might be inefficient if there are a lot of votes, but there probably won't be
-    while(items.contains(id)) {
-      id = generate
-    }
-    id
+  if(SQL.isAvailable) {
+    items = Worm.get[VoteItem]
+  } else {
+    println("Warning: Vote module will start, but existing votes will not be loaded and new votes " +
+      "will not be stored because SQL is unavailable.")
   }
 
   def onTrigger(channel: String, sender: String, login: String, hostname: String, message: String, trigger: String) = trigger match {
@@ -121,33 +82,27 @@ Add -m to !startvote to allow multiple choices from one candidate""")
         bot.msg(channel, "Please provide both a question and options.")
         return
       }
-      items = VoteItem(randomId.toString, nick, question, multi, options) :: items
+      val ni = VoteItem(nick, question, multi, options)
+      items = ni :: items
+      ni.insert
       val multiText = if(multi) " with multiple choices allowed" else ""
-      bot.msg(channel, "Created vote " + items(0).id + multiText + ".")
-      bot.msg(channel, "Type '!vote <choice> " + items(0).id + "' to vote")
-      sqlHandler.insert("insert into votes (id, creator, text, multi) values ('" + items(0).id + "', '" + nick + "', '" + question + "', '" + multi.toString + "')")
-      options foreach { o =>
-        sqlHandler.insert("insert into " + dbVoteOptions + " (voteId, text) values ('" + items(0).id + "', '" + o.text + "');")
-      }
-      // db
+      bot.msg(channel, "Created vote " + items(0).wormDbId.get + multiText + ".")
+      bot.msg(channel, "Type '!vote <choice> " + items(0).wormDbId.get + "' to vote")
     } catch {
-      case e: SQLException => bot.msg(channel, "Failed to insert the vote into SQL."); e.printStackTrace
       case e => bot.msg(channel, "Sorry, what? Maybe you should check !help vote."); e.printStackTrace
     }
   }
 
   def endVote(channel: String, id: String): Unit = {
-    if(!(items.contains(id))) {
+    val item = items.find(_ == id)
+    if(item.isEmpty) {
       bot.msg(channel, "I don't have a vote with ID \"" + id + "\", try !votes")
       return
+    } else {
+      items = items.filterNot(_ == item.get)
+      item.get.delete
+      bot.msg(channel, "Removed vote \"" + id + "\".")
     }
-    items = items.filterNot(_ == id)
-    sqlHandler.select("select id from " + dbVoteOptions + " where voteId='" + id + "';").asScala.foreach { row =>
-      sqlHandler.delete("delete from " + dbVoteOptionVoters + " where optionId='" + row(0).toString + "';")
-    }
-    sqlHandler.delete("delete from " + dbVoteOptions + " where voteId='" + id + "';")
-    sqlHandler.delete("delete from " + dbVotes + " where id='" + id + "';")
-    bot.msg(channel, "Removed vote \"" + id + "\".")
   }
 
   def showVote(channel: String, id: String): Unit = {
@@ -166,13 +121,14 @@ Add -m to !startvote to allow multiple choices from one candidate""")
       bot.msg(channel, "No votes are currently being tracked.")
     } else {
       items foreach { item =>
-        bot.msg(channel, "Vote " + item.id + " by " + item.creator + " asking \"" + item.text + "\" with " + item.resultCount + " votes")
+        bot.msg(channel, "Vote " + item.wormDbId.get + " by " + item.creator + " asking \"" +
+          item.text + "\" with " + item.resultCount + " votes")
       }
     }
   }
 
   def vote(channel: String, nick: String, message: String): Unit = {
-    val m = Pattern.compile("(.+) ([0-9]{" + idDigits + "})").matcher(message)
+    val m = Pattern.compile("(.+) ([0-9]+)").matcher(message)
     if(!(m matches) || m.groupCount < 2) {
       bot.msg(channel, "Sorry, what? Maybe you should try !help vote")
       return
@@ -188,21 +144,17 @@ Add -m to !startvote to allow multiple choices from one candidate""")
     }
     val option = item.get.options.find(_ == m.group(1))
     if(option isEmpty) {
-      bot.msg(channel, "That vote doesn't have an option called \"" + m.group(1) + "\", try !showvote " + item.get.id)
+      bot.msg(channel, "That vote doesn't have an option called \"" + m.group(1) + "\", try !showvote " + item.get.wormDbId.get)
       return
     }
     if(option.get.voters.contains(nick)) {
       bot.msg(channel, "You've already voted for that option.")
       return
     }
-    try {
-      option.get.voters = nick :: option.get.voters
-      val optionId = (sqlHandler.selectSingle("select id from " + dbVoteOptions + " where text='" + option.get.text + "' and voteId='" + m.group(2) + "';"))(0).toString
-      sqlHandler.insert("insert into " + dbVoteOptionVoters + " (optionId, nick) values ('" + optionId + "', '" + nick + "');")
-      bot.msg(channel, nick + " voted for '" + option.get.text + "' in vote " + item.get.id + ": " + item.get.text)
-    } catch {
-      case e: SQLException => bot.msg(channel, "Sorry, I failed to update votecount in SQL!"); e.printStackTrace
-    }
+    option.get.voters = nick :: option.get.voters
+    item.get.update
+    bot.msg(channel, nick + " voted for '" + option.get.text + "' in vote " + 
+      item.get.wormDbId.get + ": " + item.get.text)
   }
 
   def hasVoted(nick: String, vote: VoteItem): Boolean = vote.options.exists {
